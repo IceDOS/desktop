79ecc3ae5905212e66570ee98b718fe290f9923c
IceDBorn <git.outsider841@simplelogin.fr>
2025-11-06 20:20:18 +0200

    rework(default): support cosmic

--- modules/default/icedos.nix
+++ modules/default/icedos.nix
@@ -54,19 +54,21 @@
         }:
 
         let
-          inherit (lib) mapAttrs mkIf;
-          cfg = config.icedos;
+          inherit (icedosLib) generateAccentColor;
+          inherit (lib) mapAttrs mkIf hasAttr;
+          inherit (config.icedos) applications desktop users;
+          inherit (applications) defaultBrowser defaultEditor;
+          inherit (desktop) autologinUser gnome timezone;
 
-          accentColor = icedosLib.generateAccentColor {
-            accentColor = cfg.desktop.accentColor;
-            gnomeAccentColor = cfg.desktop.gnome.accentColor;
-            hasGnome = lib.hasAttr "gnome" cfg.desktop;
+          accentColor = generateAccentColor {
+            inherit (desktop) accentColor;
+            gnomeAccentColor = gnome.accentColor;
+            hasGnome = hasAttr "gnome" desktop;
           };
 
           audioPlayer = "io.bassi.Amberol.desktop";
-
-          browser = cfg.applications.defaultBrowser;
-          editor = cfg.applications.defaultEditor;
+          browser = defaultBrowser;
+          editor = defaultEditor;
 
           gtkCss = ''
             @define-color accent_bg_color ${accentColor};
@@ -104,116 +106,124 @@
             nerd-fonts.jetbrains-mono
           ];
 
-          time.timeZone = cfg.desktop.timezone;
+          time.timeZone = timezone;
 
           i18n = {
             defaultLocale = "en_US.UTF-8";
             extraLocaleSettings.LC_MEASUREMENT = "es_ES.UTF-8";
           };
 
-          services.displayManager.autoLogin.user = mkIf (
-            cfg.desktop.autologinUser != ""
-          ) cfg.desktop.autologinUser;
-
+          services.displayManager.autoLogin.user = mkIf (autologinUser != "") autologinUser;
           xdg.portal.config.common.default = "*";
 
-          home-manager.users = mapAttrs (user: _: {
-            gtk = {
-              enable = true;
-
-              theme = {
-                name = "adw-gtk3-dark";
-                package = pkgs.adw-gtk3;
-              };
-
-              cursorTheme = {
-                name = "Bibata-Modern-Classic";
-                package = pkgs.bibata-cursors;
-              };
-
-              iconTheme = {
-                name = "Tela-black-dark";
-                package = pkgs.tela-icon-theme;
-              };
-
-              gtk3.extraCss = gtkCss;
-            };
-
-            dconf.settings = {
-              # Enable dark mode
-              "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-
-              # GTK file picker
-              "org/gtk/settings/file-chooser" = {
-                sort-directories-first = true;
-                date-format = "with-time";
-                show-type-column = false;
-                show-hidden = true;
-              };
-            };
-
-            xdg = {
-              configFile = {
-                "gtk-4.0/gtk.css".enable = false;
-                "mimeapps.list".force = true;
-              };
-
-              # Default apps
-              mimeApps = {
+          home-manager.users =
+            let
+              inherit (pkgs) adw-gtk3 bibata-cursors tela-icon-theme;
+              hasCosmic = hasAttr "cosmic" desktop;
+              hasCosmicGtkTheming = desktop.cosmic.appearance.gtkTheming or false;
+            in
+            mapAttrs (user: _: {
+              gtk = {
                 enable = true;
 
-                defaultApplications = {
-                  "application/json" = editor;
-                  "application/pdf" = browser;
-                  "application/x-bittorrent" = "de.haeckerfelix.Fragments.desktop";
-                  "application/x-ms-dos-executable" = "wine.desktop";
-                  "application/x-shellscript" = editor;
-                  "application/x-wine-extension-ini" = editor;
-                  "application/x-zerosize" = editor;
-                  "application/xhtml_xml" = browser;
-                  "application/xhtml+xml" = browser;
-                  "application/zip" = "org.gnome.FileRoller.desktop";
-                  "audio/aac" = audioPlayer;
-                  "audio/flac" = audioPlayer;
-                  "audio/m4a" = audioPlayer;
-                  "audio/mp3" = audioPlayer;
-                  "audio/wav" = audioPlayer;
-                  "image/avif" = imageViewer;
-                  "image/jpeg" = imageViewer;
-                  "image/png" = imageViewer;
-                  "image/svg+xml" = imageViewer;
-                  "text/html" = browser;
-                  "text/plain" = editor;
-                  "video/mp4" = videoPlayer;
-                  "video/quicktime" = videoPlayer;
-                  "video/x-matroska" = videoPlayer;
-                  "video/x-ms-wmv" = videoPlayer;
-                  "x-scheme-handler/about" = browser;
-                  "x-scheme-handler/http" = browser;
-                  "x-scheme-handler/https" = browser;
-                  "x-scheme-handler/unknown" = browser;
-                  "x-www-browser" = browser;
+                theme = {
+                  name = "adw-gtk3-dark";
+                  package = adw-gtk3;
+                };
+
+                cursorTheme = {
+                  name = "Bibata-Modern-Classic";
+                  package = bibata-cursors;
+                };
+
+                iconTheme = {
+                  name = "Tela-black-dark";
+                  package = tela-icon-theme;
+                };
+
+                gtk3.extraCss = gtkCss;
+              };
+
+              dconf.settings = {
+                # Enable dark mode
+                "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+                # GTK file picker
+                "org/gtk/settings/file-chooser" = {
+                  sort-directories-first = true;
+                  date-format = "with-time";
+                  show-type-column = false;
+                  show-hidden = true;
                 };
               };
 
-              userDirs = {
-                enable = true;
-                createDirectories = true;
-              };
-            };
+              xdg = {
+                configFile = {
+                  "gtk-3.0/gtk.css".force = true;
+                  "gtk-4.0/gtk.css".enable = false;
+                  "user-dirs.dirs".force = true;
+                }
+                // (if hasCosmic then { } else { "mimeapps.list".force = true; });
 
-            home = {
-              pointerCursor = {
-                gtk.enable = true;
-                x11.enable = true;
-                package = pkgs.bibata-cursors;
-                name = "Bibata-Modern-Classic";
-                size = 24;
+                # Default apps
+                mimeApps = {
+                  enable = !hasCosmic;
+
+                  defaultApplications = {
+                    "application/json" = editor;
+                    "application/pdf" = browser;
+                    "application/x-bittorrent" = "de.haeckerfelix.Fragments.desktop";
+                    "application/x-ms-dos-executable" = "wine.desktop";
+                    "application/x-shellscript" = editor;
+                    "application/x-wine-extension-ini" = editor;
+                    "application/x-zerosize" = editor;
+                    "application/xhtml_xml" = browser;
+                    "application/xhtml+xml" = browser;
+                    "application/zip" = "org.gnome.FileRoller.desktop";
+                    "audio/aac" = audioPlayer;
+                    "audio/flac" = audioPlayer;
+                    "audio/m4a" = audioPlayer;
+                    "audio/mp3" = audioPlayer;
+                    "audio/wav" = audioPlayer;
+                    "image/avif" = imageViewer;
+                    "image/jpeg" = imageViewer;
+                    "image/png" = imageViewer;
+                    "image/svg+xml" = imageViewer;
+                    "text/html" = browser;
+                    "text/plain" = editor;
+                    "video/mp4" = videoPlayer;
+                    "video/quicktime" = videoPlayer;
+                    "video/x-matroska" = videoPlayer;
+                    "video/x-ms-wmv" = videoPlayer;
+                    "x-scheme-handler/about" = browser;
+                    "x-scheme-handler/http" = browser;
+                    "x-scheme-handler/https" = browser;
+                    "x-scheme-handler/unknown" = browser;
+                    "x-www-browser" = browser;
+                  };
+                };
+
+                userDirs = {
+                  enable = true;
+                  createDirectories = true;
+                };
               };
 
-              file.".config/gtk-4.0/gtk.css".text = gtkCss;
-            };
-          }) cfg.users;
+              home = {
+                pointerCursor = {
+                  gtk.enable = true;
+                  x11.enable = true;
+                  package = bibata-cursors;
+                  name = "Bibata-Modern-Classic";
+                  size = 24;
+                };
+
+                file.".config/gtk-4.0/gtk.css" = mkIf (!hasCosmicGtkTheming) {
+                  force = true;
+                  text = gtkCss;
+                };
+              };
+            }) users;
         }
       )
     ];
