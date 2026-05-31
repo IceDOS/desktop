b98ea12efbdab90017215b02edd7b7f4a84e4ffb
CrazyStevenz <github.ekta@aleeas.com>
2026-05-31 21:25:20 +0300

    Replace sddm with plm

--- modules/plm/icedos.nix
+++ modules/plm/icedos.nix
@@ -16,18 +16,16 @@
           inherit (config.services.displayManager) cosmic-greeter gdm;
         in
         {
-          services.displayManager.sddm = {
+          services.displayManager.plasma-login-manager = {
             enable = validate.abort {
               when = cosmic-greeter.enable || gdm.enable;
-              path = "icedos.desktop.sddm";
+              path = "icedos.desktop.plm";
               msg = "More than one display managers are setup, please use only one!";
             };
-
-            wayland.enable = true;
           };
         }
       )
     ];
 
-  meta.name = "sddm";
+  meta.name = "plm";
 }
